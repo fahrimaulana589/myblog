df7a6853e3cf21b525c51e63deeb5c272e26e7a3
akhmad fahri maulana <akhmadfahri589@gmail.com>
2022-11-27 21:26:23 +0700

    tanvahkan default table ke model

--- app/Models/SocialMedia.php
+++ app/Models/SocialMedia.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class SocialMedia extends Model
 {
     use HasFactory;
+    protected $table = "social_medias";
 
     protected $fillable = [
         "icon",
