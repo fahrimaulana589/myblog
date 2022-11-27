558db15b85150eccc39da08bed4f25fd66d0c4ce
akhmad fahri maulana <akhmadfahri589@gmail.com>
2022-11-27 20:33:09 +0700

    buat Education.php Model

--- tests/Unit/Models/EducationTest.php
+++ tests/Unit/Models/EducationTest.php
@@ -2,7 +2,8 @@
 
 namespace Tests\Unit\Models;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Education;
+use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Schema;
 use Tests\TestCase;
 
@@ -13,9 +14,34 @@ class EducationTest extends TestCase
     public function educations_database_has_expected_collumn()
     {
         $result = Schema::hasColumns("educations",[
-
+            "name","summary","date"
         ]);
 
-        $this->assertTrue(true);
+        $this->assertTrue($result);
+    }
+
+    /** @test  */
+    public function education_database_name_is_unique()
+    {
+        $this->expectException(QueryException::class);
+
+        Education::factory()->create([
+            "name" => "smk hasry"
+        ]);
+
+        Education::factory()->create([
+            "name" => "smk hasry"
+        ]);
+    }
+
+    /** @test  */
+    public function education_database_date_is_date()
+    {
+        $this->expectException(QueryException::class);
+
+        Education::factory()->create([
+            "date" => "smk hasry"
+        ]);
+
     }
 }
