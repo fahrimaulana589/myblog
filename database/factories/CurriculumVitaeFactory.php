bc0eacbef5cf692792d4f8b8c8453c88515c4a6e
akhmad fahri maulana <akhmadfahri589@gmail.com>
2022-11-29 04:28:24 +0700

    buat CurriculumVitae.php mo0del

---
+++ database/factories/CurriculumVitaeFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\CurriculumVitae;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class CurriculumVitaeFactory extends Factory
+{
+    protected $model = CurriculumVitae::class;
+
+    public function definition(): array
+    {
+        return [
+            'photo' => fake()->unique()->imageUrl(),
+            'name' => fake()->unique()->name(),
+            'summary' => fake()->text(),
+            'file' => fake()->unique()->imageUrl(),
+        ];
+    }
+}
---
+++ tests/Unit/Models/CurriculumVitaeTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+
+use App\Models\CurriculumVitae;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Schema;
+use Tests\TestCase;
+
+class CurriculumVitaeTest extends TestCase
+{
+
+    /** @test  */
+    public function curriculum_vitaes_table_has_expected_column()
+    {
+        $result = Schema::hasColumns("curriculum_vitaes",[
+           "name","photo","summary","file"
+        ]);
+
+        $this->assertTrue($result);
+    }
+
+    /** @test  */
+    public function curriculum_vitaes_table_name_column_is_unique()
+    {
+        $this->expectException(QueryException::class);
+
+        CurriculumVitae::factory()->create([
+            "name" => "fahri"
+        ]);
+
+        CurriculumVitae::factory()->create([
+            "name" => "fahri"
+        ]);
+    }
+
+    /** @test  */
+    public function curriculum_vitaes_table_photo_column_is_unique()
+    {
+        $this->expectException(QueryException::class);
+
+        CurriculumVitae::factory()->create([
+            "photo" => "default.png"
+        ]);
+
+        CurriculumVitae::factory()->create([
+            "photo" => "default.png"
+        ]);
+    }
+
+    /** @test  */
+    public function curriculum_vitaes_table_file_column_is_unique()
+    {
+        $this->expectException(QueryException::class);
+
+        CurriculumVitae::factory()->create([
+            "file" => "default.png"
+        ]);
+
+        CurriculumVitae::factory()->create([
+            "file" => "default.png"
+        ]);
+    }
+}
