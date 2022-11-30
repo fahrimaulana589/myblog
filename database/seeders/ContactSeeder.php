f594d0664d8289f28b19151f4dbef228094b2334
akhmad fahri maulana <akhmadfahri589@gmail.com>
2022-12-01 05:07:42 +0700

    membuat seed ContactSeeder.php

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -2,12 +2,16 @@
 
 namespace Database\Seeders;
 
+use App\Models\Contact;
 use Illuminate\Database\Seeder;
 
 class ContactSeeder extends Seeder
 {
     public function run()
     {
-
+        Contact::factory()->create([
+           "name" => "Akhmad Fahri Maulana",
+           "email" => "akhmadfahri589@gmail.com",
+        ]);
     }
 }
