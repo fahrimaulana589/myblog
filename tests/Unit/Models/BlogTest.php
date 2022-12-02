65cb50d121fcfa5a0ca342ca32bd4350ee1c31e0
fahri <akhmadfahri589@gmail.com>
2022-12-02 09:32:54 +0700

    pembuatan test untuk blog bagian tags

--- tests/Unit/Models/BlogTest.php
+++ tests/Unit/Models/BlogTest.php
@@ -6,6 +6,7 @@ use App\Models\Blog;
 use App\Models\Category;
 use App\Models\Tag;
 use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Tests\TestCase;
 
@@ -169,7 +170,52 @@ class BlogTest extends TestCase
         ]);
 
         $blog->tags()->attach([2, 9]);
+    }
 
+    /** @test  */
+    public function pivot_yang_menghubungkan_blog_dan_tag_akan_dihapus_jika_blog_dihapus()
+    {
+        Tag::factory(5)->create()->first();
+
+        $category = Category::factory()->create();
+
+        $blog = Blog::factory()->create([
+            "category_id" => $category->id
+        ]);
+
+        $tags = Tag::all()->map(function ($val) {
+            return $val->id;
+        })->toArray();
+        $blog->tags()->attach($tags);
+
+        Blog::find($blog->id)->delete();
+
+        $pivot = DB::table("blog_tag")->get();
+
+        $this->assertTrue($pivot->count() == 0);
+    }
+
+    /** @test  */
+    public function pivot_yang_menghubungkan_blog_dan_tag_akan_dihapus_jika_tag_dihapus()
+    {
+        $tag = Tag::factory()->create()->first();
+
+        $category = Category::factory()->create();
+
+        $blog = Blog::factory(5)->create([
+            "category_id" => $category->id
+        ]);
+
+        $blogs = Blog::all()->map(function ($val) {
+            return $val->id;
+        })->toArray();
+        $tag->blogs()->attach($blogs);
+
+        Tag::find($tag->id)->delete();
+
+        $pivot = DB::table("blog_tag")->get();
+
+        $this->assertTrue($pivot->count() == 0);
     }
 
 }
