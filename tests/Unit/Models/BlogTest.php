b16853c5508d4f18e3f8744823115f47e3009a77
fahri <akhmadfahri589@gmail.com>
2022-12-02 09:14:04 +0700

    pembuatan test untuk blog

--- tests/Unit/Models/BlogTest.php
+++ tests/Unit/Models/BlogTest.php
@@ -4,23 +4,24 @@ namespace Models;
 
 use App\Models\Blog;
 use App\Models\Category;
+use App\Models\Tag;
 use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Schema;
 use Tests\TestCase;
 
 class BlogTest extends TestCase
 {
-    /** @test  */
+    /** @test */
     public function blogs_table_has_expected_column()
     {
-        $result = Schema::hasColumns("blogs",[
-            "name","image","date","content","comment"
+        $result = Schema::hasColumns("blogs", [
+            "name", "image", "date", "content", "comment"
         ]);
 
         $this->assertTrue($result);
     }
 
-    /** @test  */
+    /** @test */
     public function blogs_table_name_is_unique()
     {
         $this->expectException(QueryException::class);
@@ -34,7 +35,7 @@ class BlogTest extends TestCase
         ]);
     }
 
-    /** @test  */
+    /** @test */
     public function blogs_table_image_is_unique()
     {
         $this->expectException(QueryException::class);
@@ -48,7 +49,7 @@ class BlogTest extends TestCase
         ]);
     }
 
-    /** @test  */
+    /** @test */
     public function blogs_table_comment_is_unique()
     {
         $this->expectException(QueryException::class);
@@ -62,7 +63,7 @@ class BlogTest extends TestCase
         ]);
     }
 
-    /** @test  */
+    /** @test */
     public function blog_punya_satu_categori()
     {
         Category::factory(5)->create();
@@ -76,7 +77,7 @@ class BlogTest extends TestCase
         $this->assertTrue($result);
     }
 
-    /** @test  */
+    /** @test */
     public function blog_dapat_dibuat_jika_kategori_id_ada()
     {
         $category = Category::factory(5)->create()->first();
@@ -85,10 +86,10 @@ class BlogTest extends TestCase
             "category_id" => $category->id
         ]);
 
-        $this->assertDatabaseHas("blogs",$blog->toArray());
+        $this->assertDatabaseHas("blogs", $blog->toArray());
     }
 
-    /** @test  */
+    /** @test */
     public function blog_tidak_dapat_dibuat_jika_kategori_tidak_ada()
     {
         $this->expectException(QueryException::class);
@@ -101,7 +102,7 @@ class BlogTest extends TestCase
     }
 
     /** @test */
-    public function categori_tidak_bisa_dihapus_jika_blog_ada_yang_memmiliki_idnya()
+    public function categori_tidak_bisa_dihapus_jika_blog_ada_yang_memiliki_idnya()
     {
         $this->expectException(QueryException::class);
 
@@ -114,5 +115,61 @@ class BlogTest extends TestCase
         Category::find(5)->delete();
     }
 
+    /** @test */
+    public function blog_memiliki_banyak_tags()
+    {
+        Tag::factory(5)->create()->first();
+
+        $category = Category::factory()->create();
+
+        $blog = Blog::factory()->create([
+            "category_id" => $category->id
+        ]);
+
+        $blog->tags()->attach([2, 4]);
+
+        $tags = Blog::find($blog->id)->tags();
+
+        $this->assertTrue($tags->count() == 2);
+    }
+
+    /** @test */
+    public function blog_dapat_dibuat_jika_tag_id_ada_di_table_tags()
+    {
+        Tag::factory(5)->create();
+
+        $category = Category::factory()->create();
+
+        $blog = Blog::factory()->create([
+            "category_id" => $category->id
+        ]);
+
+        $tags = Tag::all()->map(function ($val) {
+            return $val->id;
+        })->toArray();
+
+        $blog->tags()->attach($tags);
+
+        $tags = Blog::all()->first()->tags;
+
+        $this->assertTrue($tags->count() == 5);
+    }
+
+    /** @test */
+    public function blog_tidak_dapat_dibuat_jika_tag_id_tidak_ada_di_table_tags()
+    {
+        $this->expectException(QueryException::class);
+
+        Tag::factory(5)->create();
+
+        $category = Category::factory()->create();
+
+        $blog = Blog::factory()->create([
+            "category_id" => $category->id
+        ]);
+
+        $blog->tags()->attach([2, 9]);
+
+    }
 
 }
