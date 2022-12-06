bb22e0987de0f7174fe7e74af1dec1a49e0cfc1d
akhmad fahri maulana <akhmadfahri589@gmail.com>
2022-12-06 20:45:33 +0700

    buat test untuk sosial media

---
+++ tests/Unit/Repositories/SosialMediaRepositoryTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Repositories;
+
+use PHPUnit\Framework\TestCase;
+
+class SosialMediaRepositoryTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Unit/Services/SosialMediaServiceTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Services;
+
+use PHPUnit\Framework\TestCase;
+
+class SosialMediaServiceTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
