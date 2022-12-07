0567a33ae04c9e435a89398bcc775a173ac9bc79
akhmad fahri maulana <akhmadfahri589@gmail.com>
2022-12-08 05:40:11 +0700

    refactory code

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,16 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Storage;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication,RefreshDatabase;
+
+    protected function afterRefreshingDatabase()
+    {
+        Storage::deleteDirectory('');
+
+    }
+
 }
