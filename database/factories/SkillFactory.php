7be1702f0613b7e1e23f77f4de943f5a0f461680
akhmadfahri <akhmadfahri@gmail.com>
2023-01-29 14:45:44 +0700

    mengubah default file

--- database/factories/SkillFactory.php
+++ database/factories/SkillFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Skill;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Http\UploadedFile;
 
 class SkillFactory extends Factory
 {
@@ -12,7 +13,7 @@ class SkillFactory extends Factory
     public function definition(): array
     {
         return [
-            'icon' => fake()->unique()->imageUrl(),
+            'icon' => UploadedFile::fake()->image('avatar.jpg')->store('files'),
             'name' => fake()->unique()->name,
         ];
     }
