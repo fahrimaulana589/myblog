c4160ed5711bc40a6e1f3d38f035e15f2fe9c8e3
akhmadfahri <akhmadfahri@gmail.com>
2023-01-29 21:55:10 +0700

    membuat test education

---
+++ tests/Unit/Repositories/EducationRepositoryTest.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace Tests\Unit\Repositories;
+
+use App\Models\Education;
+use App\Repositories\Education\EducationRepository;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
+use Tests\TestCase;
+
+class EducationRepositoryTest extends TestCase
+{
+    public function test_ambil_semua_education(){
+        Education::factory(5)->create();
+
+        $educationRepository = app()->make(EducationRepository::class);
+
+        $educations = $educationRepository->all();
+
+        $this->assertTrue($educations->count() == 5);
+    }
+
+    public function test_ambil_detail_education_dengan_id(){
+        $educations = Education::factory(5)->create();
+
+        $educationRepository = app()->make(EducationRepository::class);
+
+        $education = $educationRepository->find($educations->first()->id);
+
+        $this->assertTrue($education->id == $educations->first()->id);
+    }
+
+    public function test_ambil_detail_education_dengan_id_tidak_ada(){
+        $this->expectException(ModelNotFoundException::class);
+
+        Education::factory(5)->create();
+
+        $educationRepository = app()->make(EducationRepository::class);
+
+        $educationRepository->findOrFail(121);
+    }
+
+
+    public function test_buat_data_education()
+    {
+        $educationRepository = app()->make(EducationRepository::class);
+
+        $education = $educationRepository->create([
+            'name' => 'test',
+            'summary' => 'test',
+            'date' => '2020-06-01 00:00:00',
+        ]);
+
+        $this->assertDatabaseCount("educations",1);
+    }
+
+    public function test_buat_data_education_dengan_nama_sama_akan_eror()
+    {
+        $this->expectException(QueryException::class);
+
+        $educationRepository = app()->make(EducationRepository::class);
+
+        $nama = 'php';
+
+        $education = $educationRepository->create([
+            'name' => 'test',
+            'summary' => 'test',
+            'date' => 'test',
+        ]);
+
+        $education = $educationRepository->create([
+            'name' => 'test',
+            'summary' => 'test2',
+            'date' => 'test2',
+        ]);
+    }
+
+    public function test_buat_data_education_dengan_nama_tidak_ada_akan_eror()
+    {
+        $this->expectException(QueryException::class);
+
+        $educationRepository = app()->make(EducationRepository::class);
+
+        $education = $educationRepository->create([
+            'summary' => 'test2',
+            'date' => 'test2',
+        ]);
+    }
+
+    public function test_buat_data_education_dengan_summary_tidak_ada_akan_eror()
+    {
+        $this->expectException(QueryException::class);
+
+        $educationRepository = app()->make(EducationRepository::class);
+
+        $education = $educationRepository->create([
+            'name' => 'test',
+            'date' => 'test2',
+        ]);
+    }
+
+    public function test_buat_data_education_dengan_date_tidak_ada_akan_eror()
+    {
+        $this->expectException(QueryException::class);
+
+        $educationRepository = app()->make(EducationRepository::class);
+
+        $education = $educationRepository->create([
+            'name' => 'test',
+            'summary' => 'test2',
+        ]);
+    }
+
+    public function test_update_data_education_dengan_id()
+    {
+        $idEducation = Education::factory(5)->create()->first()->id;
+
+        $educationRepository = app()->make(EducationRepository::class);
+
+        $education = $educationRepository->update($idEducation,[
+            'name' => 'test',
+            'summary' => 'test2',
+            'date' => '2020-06-01 00:00:00',
+        ]);
+
+        $this->assertDatabaseHas('educations',[
+            'name' => 'test',
+            'summary' => 'test2',
+            'date' => '2020-06-01 00:00:00',
+        ]);
+    }
+
+    public function test_update_data_education_dengan_id_tidak_ada_akan_eror()
+    {
+        $this->expectException(ModelNotFoundException::class);
+
+        $idEducation = Education::factory(5)->create()->first()->id;
+
+        $educationRepository = app()->make(EducationRepository::class);
+
+        $education = $educationRepository->update(121,[
+            'name' => 'test',
+            'summary' => 'test2',
+            'date' => 'test2',
+        ]);
+    }
+
+    public function test_update_data_education_dengan_nama_sudah_ada_akan_eror()
+    {
+        $this->expectException(QueryException::class);
+
+        $data1 = Education::factory()->create([
+            'name' => 'test',
+            'summary' => 'test2',
+            'date' => 'test2',
+        ]);
+        $data2 = Education::factory()->create();
+
+        $educationRepository = app()->make(EducationRepository::class);
+
+        $education = $educationRepository->update($data2->id,[
+            'name' => 'test',
+            'summary' => 'test2',
+            'date' => 'test2',
+        ]);
+    }
+
+    public function test_update_data_education_dengan_icon_sudah_ada_akan_eror()
+    {
+        $this->expectException(QueryException::class);
+
+        $data1 = Education::factory()->create([
+            'icon' => 'teasst.jpg'
+        ]);
+        $data2 = Education::factory()->create();
+
+        $educationRepository = app()->make(EducationRepository::class);
+
+        $education = $educationRepository->update($data2->id,[
+            'icon' => 'teasst.jpg'
+        ]);
+    }
+
+    public function test_delete_education_dari_id()
+    {
+        $education = Education::factory()->create();
+
+        $educationRepository = app()->make(EducationRepository::class);
+
+        $educationRepository->delete($education->id);
+
+        $this->assertDatabaseCount('educations',0);
+    }
+
+    public function test_delete_education_dari_id_tidak_ada_akan_eror()
+    {
+        $this->expectException(ModelNotFoundException::class);
+
+        $educationRepository = app()->make(EducationRepository::class);
+
+        $educationRepository->delete(121212);
+
+        $this->assertDatabaseCount('educations',0);
+    }
+}
---
+++ tests/Unit/Services/EducationServiceTest.php
@@ -0,0 +1,239 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use App\Models\Education;
+use App\Repositories\Education\EducationRepository;
+use App\Services\Education\EducationService;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class EducationServiceTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+
+    /** @test */
+    public function ambil_semua_education()
+    {
+        Education::factory(5)->create();
+
+        $educationService = app()->make(EducationService::class);
+
+        $educations = $educationService->all();
+
+        $this->assertTrue($educations->count() == 5);
+    }
+
+    /** @test */
+    public function ambil_detail_data_education_dengan_id()
+    {
+        $idEducation = Education::factory(5)->create()->first()->id;
+
+        $educationService = app()->make(EducationService::class);
+
+        $socialMedia = $educationService->findOrFail($idEducation);
+
+        $this->assertTrue($socialMedia->id == $idEducation);
+    }
+
+    /** @test */
+    public function ambil_detail_data_education_dengan_id_tidak_ada_akan_eror()
+    {
+        $this->expectException(ModelNotFoundException::class);
+
+        Education::factory(5)->create()->first()->id;
+
+        $educationService = app()->make(EducationService::class);
+
+        $educationService->findOrFail(155);
+    }
+
+    /** @test */
+    public function buat_data_education()
+    {
+        $educationService = app()->make(EducationService::class);
+
+        $request = Request::create('/', 'POST', [
+            'name' => 'test',
+            'summary' => 'test2',
+            'date' => '2020-06-01 00:00:00',
+        ]);
+
+        request()->request = $request;
+
+        $educationService->create($request->all());
+
+        $this->assertDatabaseCount('educations', 1);
+    }
+
+    /** @test */
+    public function buat_data_duplikat_education_nama_sama_akan_eror()
+    {
+        $this->expectException(QueryException::class);
+
+        $educationService = app()->make(EducationService::class);
+
+        $request = Request::create('/', 'POST', [
+            'name' => 'test',
+            'summary' => 'test',
+            'date' => '2020-06-01 00:00:01',
+        ]);
+
+        $request2 = Request::create('/', 'POST', [
+            'name' => 'test',
+            'summary' => 'test2',
+            'date' => '2020-06-01 00:00:02',
+        ]);
+
+        request()->request = $request;
+        $educationService->create($request->all());
+
+        request()->request = $request2;
+        $educationService->create($request2->all());
+    }
+
+    /** @test */
+    public function buat_data_education_tanpa_nama_akan_eror()
+    {
+        $this->expectException(QueryException::class);
+
+        $educationService = app()->make(EducationService::class);
+
+        $request = Request::create('/', 'POST', [
+            'summary' => 'test2',
+            'date' => '2020-06-01 00:00:02',
+        ]);
+
+        request()->request = $request;
+
+        $data = $educationService->create($request->all());
+
+    }
+
+    /** @test */
+    public function buat_data_education_tanpa_summary_akan_eror()
+    {
+        $this->expectException(QueryException::class);
+
+        $educationService = app()->make(EducationService::class);
+
+        $request = Request::create('/', 'POST', [
+            'name' => 'test',
+            'date' => '2020-06-01 00:00:02',
+        ]);
+
+        request()->request = $request;
+
+        $educationService->create($request->all());
+
+    }
+
+    /** @test */
+    public function buat_data_education_tanpa_date_akan_eror()
+    {
+        $this->expectException(QueryException::class);
+
+        $educationService = app()->make(EducationService::class);
+
+        $request = Request::create('/', 'POST', [
+            'name' => 'test',
+            'summary' => 'test2',
+        ]);
+
+        request()->request = $request;
+
+        $educationService->create($request->all());
+
+    }
+
+    /** @test */
+    public function update_data_education_dengan_id()
+    {
+        $education = Education::factory(5)->create()->first();
+
+        $educationService = app()->make(EducationService::class);
+
+        $request = Request::create('/', 'POST', [
+            'name' => 'test',
+            'summary' => 'test2',
+            'date' => '2020-06-01',
+        ]);
+
+        request()->request = $request;
+
+        $educationService->update($education->id, $request->all());
+
+        $this->assertDatabaseHas('educations', [
+            'name' => 'test',
+            'summary' => 'test2',
+            'date' => '2020-06-01',
+        ]);
+    }
+
+    /** @test */
+    public function update_data_education_dengan_id_tidak_ada()
+    {
+        $this->expectException(ModelNotFoundException::class);
+
+        $educationRepository = app()->make(EducationRepository::class);
+
+        $educationRepository->update(6, [
+            'name' => 'test',
+            'summary' => 'test2',
+            'date' => '2020-06-01 00:00:02',
+        ]);
+    }
+
+    /** @test */
+    public function update_data_education_dengan_nama_sama_akan_eror()
+    {
+        $this->expectException(QueryException::class);
+
+        Education::factory()->create([
+            'name' => "popo"
+        ]);
+        $education = Education::factory()->create();
+
+        $educationService = app()->make(EducationService::class);
+
+        $request = Request::create('/', 'POST', [
+            'name' => 'popo',
+            'summary' => 'test2',
+            'date' => '2020-06-01',
+        ]);
+
+        request()->request = $request;
+
+        $educationService->update($education->id, $request->all());
+    }
+
+    /** @test */
+    public function delete_data_education_dengan_id()
+    {
+        $sosialMedai = Education::factory()->create();
+
+        $educationService = app()->make(EducationService::class);
+
+        $educationService->delete($sosialMedai->id);
+
+        $this->assertDatabaseCount('educations', 0);
+    }
+
+    /** @test */
+    public function delete_data_education_dengan_id_tidak_ada_akan_eror()
+    {
+        $this->expectException(ModelNotFoundException::class);
+
+        $educationService = app()->make(EducationService::class);
+
+        $educationService->delete(3);
+    }
+}
